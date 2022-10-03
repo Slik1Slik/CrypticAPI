1988d453cc66cddf631842d772a725fccec4438c
Slik <chesto.newsc@ya.ru>
2022-10-04 00:36:39 +0300

    Add representation of Home models as HomeViewModelRepresentation struct

---
+++ Cryptic API/Home MVVM/Shared/HomeViewModelRepresentation.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModelRepresentation.swift
+//  Cryptic API
+//
+//  Created by Slik on 02.10.2022.
+//
+
+import Foundation
+
+struct HomeViewModelRepresentation {
+    var title: String
+    var details: String
+}
