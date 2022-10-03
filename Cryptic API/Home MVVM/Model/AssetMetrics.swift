6a13837a93242b67ba247ec29ec5b7b4cdf32463
Slik <chesto.newsc@ya.ru>
2022-10-04 00:33:16 +0300

    Add AssetMetrics model

---
+++ Cryptic API/Home MVVM/Model/AssetMetrics.swift
@@ -0,0 +1,19 @@
+//
+//  AssetMetrcis.swift
+//  Cryptic API
+//
+//  Created by Slik on 02.10.2022.
+//
+
+import Foundation
+
+struct AssetMetrics : Decodable {
+    
+    var symbol: String = ""
+    var name: String = ""
+    var marketData: AssetMarketData = .init()
+    
+    enum CodingKeys: String, CodingKey {
+        case marketData = "market_data"
+    }
+}
