cc461c5e0043807756759de6dad7f6181731c516
Slik <chesto.newsc@ya.ru>
2022-10-07 10:37:43 +0300

    Add MVVMModuleBuilder protocol

---
+++ Cryptic API/MVVM Module Building/MVVMModuleBuilder.swift
@@ -0,0 +1,12 @@
+//
+//  MVVMModuleBuilder.swift
+//  Cryptic API
+//
+//  Created by Slik on 04.10.2022.
+//
+
+import UIKit
+
+protocol MVVMModuleBuilder {
+    func build() -> UIViewController
+}
